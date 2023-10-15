e447dc9331af2d66f30d126b14949ce400341763
WeWebD <info@wewebdesign.co.uk>
2023-10-15 18:38:36 +0100

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import SetAppBackground from "./helpers/SetAppBackground";
+import NameSearchbar from "./components/NameSearchbar";
+import CurrentWeather from "./components/CurrentWeather";
+import HourlyWeatherForecast from "./components/HourlyWeatherForecast";
+import SevenDayForecast from "./components/SevenDayForecast";
+import Navbar from "./components/Navbar";
+import AirPollution from "./components/AirPollution";
+import UvIndexSunset from "./components/UvIndexSunset";
+import WindPressure from "./components/WindPressure";
+import FeelsLikeHumidity from "./components/FeelsLikeHumidity";
+import PercipitationVisibility from "./components/PercipitationVisibility";
+import ErrorReport from "./components/ErrorReport";
+import { useEffect } from "react";
+import useWeatherContext from "./hooks/useWeatherContext";
 
 function App() {
+  const { error, loadWeather } = useWeatherContext();
+
+  useEffect(() => {
+    loadWeather();
+  }, [loadWeather]);
+
+  const theme = SetAppBackground();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`flex  ${theme} text-skin-textDefault justify-center `}>
+      <div className="h-screen w-screen bg-gradient-to-b from-skin-gradientStart via-skin-gradientMid to-skin-gradientEnd fixed top-0 z-0 max-w-md"></div>
+      <div className="fixed top-0 w-full text-center z-40 bg-gradient-to-b from-skin-gradientStart via-skin-gradientStart to-transperent max-w-md pt-16">
+        <NameSearchbar />
+        {error ? <ErrorReport message={error} /> : ""}
+      </div>
+
+      <div className="w-screen font-roboto px-5 pb-32 pt-32 z-0 max-w-md">
+        <CurrentWeather />
+        <HourlyWeatherForecast />
+        <SevenDayForecast />
+        <FeelsLikeHumidity />
+        <AirPollution />
+        <UvIndexSunset />
+        <WindPressure />
+        <PercipitationVisibility />
+      </div>
+
+      <Navbar />
     </div>
   );
 }
